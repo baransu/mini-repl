8f4d7539a16af9ef4cc9f6f75e93367210039a57
Tomasz Cichocinski <tomaszcichocinski@gmail.com>
2016-10-19 16:37:01 +0200

    infix notation FTW

--- main.c
+++ main.c
@@ -8,15 +8,15 @@
 #include "math.h"
 
 void onp(char* rownanie) {
-  Stack stos;
-  init(&stos);
+  DoubleStack stos;
+  d_init(&stos);
   const int len = strlen(rownanie);
   for(unsigned int i = 0; i < len; i++) {
     char c = rownanie[i];
-    if((c == '+' || c == '/' || c == '-' || c == '*' || c == '^') && size(&stos) > 1) {
+    if((c == '+' || c == '/' || c == '-' || c == '*' || c == '^') && d_size(&stos) > 1) {
       double wynik;
-      double snd = pop(&stos);
-      double fst = pop(&stos);
+      double snd = d_pop(&stos);
+      double fst = d_pop(&stos);
       switch(c) {
       case '+':
         wynik = fst + snd;
@@ -39,11 +39,11 @@ void onp(char* rownanie) {
         break;
       }
 
-      push(&stos, wynik);
+      d_push(&stos, wynik);
     } else if (c >= 'a' && c <= 'z') {
       char function[64] = {' '};
       int iterator = 0;
-      
+
       while(i < len && c >= 'a' && c <= 'z') {
         function[iterator++] = rownanie[i++];
         c = rownanie[i];
@@ -51,55 +51,55 @@ void onp(char* rownanie) {
 
       double wynik;
       if(strstr(function, "add") != 0) {
-        double snd = pop(&stos);
-        double fst = pop(&stos);
+        double snd = d_pop(&stos);
+        double fst = d_pop(&stos);
         wynik = fst + snd;
-        push(&stos, wynik);
+        d_push(&stos, wynik);
       } else if(strstr(function, "subtract") != 0) {
-        double snd = pop(&stos);
-        double fst = pop(&stos);
+        double snd = d_pop(&stos);
+        double fst = d_pop(&stos);
         wynik = fst - snd;
-        push(&stos, wynik);
+        d_push(&stos, wynik);
       } else if(strstr(function, "multiply") != 0) {
-        double snd = pop(&stos);
-        double fst = pop(&stos);
+        double snd = d_pop(&stos);
+        double fst = d_pop(&stos);
         wynik = fst * snd;
-        push(&stos, wynik);
+        d_push(&stos, wynik);
       } else if(strstr(function, "divide") != 0) {
-        double snd = pop(&stos);
-        double fst = pop(&stos);
+        double snd = d_pop(&stos);
+        double fst = d_pop(&stos);
         if(snd == 0) {
           printf("Cannot divide by 0!!!\n");
           return;
         }
         wynik = fst / snd;
-        push(&stos, wynik);
+        d_push(&stos, wynik);
       } else if(strstr(function, "pow") != 0) {
-        double snd = pop(&stos);
-        double fst = pop(&stos);
+        double snd = d_pop(&stos);
+        double fst = d_pop(&stos);
         wynik = power(fst, snd);
-        push(&stos, wynik);
+        d_push(&stos, wynik);
       } else if(strstr(function, "neg") != 0) {
-        double fst = pop(&stos);
+        double fst = d_pop(&stos);
         wynik = -fst;
-        push(&stos, wynik);
+        d_push(&stos, wynik);
       } else if(strstr(function, "root") != 0) {
-        double fst = pop(&stos);
-        double snd = pop(&stos);
+        double fst = d_pop(&stos);
+        double snd = d_pop(&stos);
         wynik = root(fst, snd);
-        push(&stos, wynik);
+        d_push(&stos, wynik);
       } else if(strstr(function, "sqrt") != 0) {
-        push(&stos, root(pop(&stos), 2));
+        d_push(&stos, root(d_pop(&stos), 2));
       } else if(strstr(function, "factorial") != 0) { // factorial
-        push(&stos, factorial(pop(&stos)));
+        d_push(&stos, factorial(d_pop(&stos)));
       } else if(strstr(function, "sin") != 0) { // sine
-        push(&stos, sine(pop(&stos)));
+        d_push(&stos, sine(d_pop(&stos)));
       } else if(strstr(function, "cos") != 0) { // cosine
-        push(&stos, cosine(pop(&stos)));
+        d_push(&stos, cosine(d_pop(&stos)));
       } else if(strstr(function, "rad") != 0) { // convert to radians
-        push(&stos, to_radians(pop(&stos)));
+        d_push(&stos, to_radians(d_pop(&stos)));
       } else if(strstr(function, "deg") != 0) { // convert to degrees
-        push(&stos, to_degrees(pop(&stos)));
+        d_push(&stos, to_degrees(d_pop(&stos)));
       }
 
     } else if ((c >= '0' && c <= '9') || c == '.') {
@@ -111,19 +111,100 @@ void onp(char* rownanie) {
       }
       double val = 0;
       sscanf(number, "%lf", &val);
-      push(&stos, val);
+      d_push(&stos, val);
     }
   }
-  printf("%f\n", pop(&stos));
+
+  printf("%f\n", d_pop(&stos));
 }
 
-int main () {
+void infix(char* wejscie) {
+  CharStack stos;
+  c_init(&stos);
+  const unsigned int len = strlen(wejscie);
+  char wyjscie[1024] = {' '};
+  unsigned int iterator = 0;
+  for(int i = 0; i < len; i++) {
+    char c = wejscie[i];
+    if(c == '(') {
+        c_push(&stos, c);
+    }
+
+    else if (c == ')') {
+      while(c_size(&stos) > 0) {
+        char top = c_pop(&stos);
+        if(top != '(') {
+          wyjscie[iterator++] = top;
+          wyjscie[iterator++] = ' ';
+        } else break;
+      }
+    }
+
+    else if (c == '+' || c == '-') {
+      while(c_size(&stos) > 0) {
+        char o2 = c_back(&stos);
+        if(o2 == '+' || o2 == '-' || o2 == '*' || o2 == '/') {
+          wyjscie[iterator++] = o2;
+          wyjscie[iterator++] = ' ';
+          c_pop(&stos);
+        } else break;
+      }
+      c_push(&stos, c);
+    }
+
+    else if (c == '*' || c == '/') {
+      while(c_size(&stos) > 0) {
+        char o2 = c_back(&stos);
+        if(o2 == '*' || o2 == '/') {
+          wyjscie[iterator++] = o2;
+          wyjscie[iterator++] = ' ';
+          c_pop(&stos);
+        } else break;
+      }
+      c_push(&stos, c);
+    }
+
+    else if (c >= '0' && c <= '9') {
+      // TODO liczby zmienno-przecinkowe
+      char number[256] = {' '};
+      unsigned int ii = 0;
+      do {
+        number[ii++] = c;
+        c = wejscie[++i];
+      } while(i < len - 1 && c >= '0' && c <= '9');
+      --i;
+      const unsigned int ln = strlen(number);
+      for(unsigned int i = 0; i < ln; i++) {
+        wyjscie[iterator++] = number[i];
+      }
+      wyjscie[iterator++] = ' ';
+    }
+
+    else if (c >= 'a' && c <= 'z') { // funkcje
+      // TODO zczytaj string
+      // TODO stworz stos funckji
+      // TODO wsadz string na stos
+    }
+  }
+
+  while(c_size(&stos) > 0) {
+    wyjscie[iterator++] = c_pop(&stos);
+  }
+
+  printf("ONP: %s\n", wyjscie);
+  onp(wyjscie);
+}
+
+int main (int argc, char* argv) {
+  printf("args: %s\n", argv);
   char rownanie[256] = {' '};
   // TODO some command about entering REPL mode or something
+  // TODO infix notation
   printf("> ");
 
   while(fgets(rownanie, sizeof(rownanie), stdin) != NULL && strstr(rownanie, "exit") == 0 && strlen(rownanie) > 1) {
-    onp(rownanie);
+    infix(rownanie);
+    //onp(rownanie);
     printf("> ");
   }
 
--- stack.h
+++ stack.h
@@ -1,27 +1,70 @@
-struct Stack {
+// DOUBLE STACK
+
+struct DoubleStack {
   int top;
   double stack[256];
 };
-typedef struct Stack Stack;
+typedef struct DoubleStack DoubleStack;
 
-void init(Stack *stos) {
+void d_init(DoubleStack *stos) {
   stos->top = 0;
 }
 
-int isEmpty(Stack* stos) {
+int d_isEmpty(DoubleStack* stos) {
   return stos->top;
 }
 
-double pop(Stack* stos) {
+double d_pop(DoubleStack* stos) {
+  const double last = stos->stack[stos->top];
   stos->top -= 1;
+  return last;
+}
+
+void d_push(DoubleStack* stos, double wartosc) {
+  stos->top += 1;
+  stos->stack[stos->top] = wartosc;
+}
+
+int d_size(DoubleStack* stos) {
+  return stos->top;
+}
+
+
+// CHAR* STACK
+
+struct CharStack {
+  int top;
+  char stack[1024];
+};
+typedef struct CharStack CharStack;
+
+void c_init(CharStack *stos) {
+  stos->top = 0;
+  for(unsigned short i = 0; i < 1024; i++) {
+    stos->stack[i] = ' ';
+  }
+}
+
+int c_isEmpty(CharStack* stos) {
+  return stos->top;
+}
+
+char c_pop(CharStack* stos) {
+  const char last = stos->stack[stos->top];
+  stos->top -= 1;
+  return last;
+}
+
+char c_back(CharStack* stos) {
   return stos->stack[stos->top];
 }
 
-void push(Stack* stos, double wartosc) {
-  stos->stack[stos->top] = wartosc;
+
+void c_push(CharStack* stos, char wartosc) {
   stos->top += 1;
+  stos->stack[stos->top] = wartosc;
 }
 
-int size(Stack* stos) {
+int c_size(CharStack* stos) {
   return stos->top;
 }
